2d9b2c6853ac150ee91de37a9af348dbb7dbbd15
Sebastian <sebastian.leonowicz5@gmail.com>
2019-02-05 16:39:59 +0100

    adds Home component

--- ticketing-app/src/App.js
+++ ticketing-app/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home/Home'
 
 class App extends Component {
-
+  state = {
+    loggedIn: 'false'
+  }
 
   render() {
     return (
       <div className="App">
-     
+       <Home></Home>
       </div>
     );
   }
---
+++ ticketing-app/src/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const home = () => {
+return <div>
+            <h3>
+                Welcome to the ticketing app
+            </h3>
+            <ul>
+            <li>Create a ticket or delete one</li>
+            <li>Organize your work</li>
+            </ul>
+            <p>
+            Sign In to start
+            </p>
+        </div>
+}
+
+export default home;
