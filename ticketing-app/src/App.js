313794e827199e5f37b5f343198155181a6fd2b2
Sebastian <sebastian.leonowicz5@gmail.com>
2019-02-06 10:49:10 +0100

    changes navigation into a separate component

--- ticketing-app/src/App.js
+++ ticketing-app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './Home/Home'
+import './Components/Home/Home.css'
+import Navigation from './Components/Navigation/Navigation'
+import './Components/Navigation/Navigation.css'
 
 class App extends Component {
   state = {
@@ -10,7 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <Home></Home>
+        <Navigation></Navigation>
+        <Home></Home>
       </div>
     );
   }
---
+++ ticketing-app/src/Components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const home = () => {
+return <div className='homeComponent'>
+            <h3 className='homeHeader'>
+                Welcome to the ticketing app
+            </h3>
+            <p className='homeParagraph'>
+                Create a ticket or delete one
+            </p>
+            <p className='homeParagraph'>
+                Organize your work
+            </p>
+            <p className='homeParagraph'>
+                Sign In to start
+            </p>
+        </div>
+}
+
+export default home;
